e16b2d322d06e58629a7a94cfda3db7784ea0dd3
huilong.zhu <zhuhuilong@zj.tech>
2023-05-23 14:52:08 +0800

    feat: 暗黑模式逻辑

---
+++ src/pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className=" bg-gray-10 dark:bg-gray-800">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,33 +4,33 @@ import { fetchArticles, fetchTopArticles } from "@/services";
 import ArtilcleListItem from "@/components/ArticleListItem";
 import ArticleRankingList from "@/components/ArticleRankingList";
 
-export default function Home({ posts ,topPosts}) {
+export default function Home({ posts, topPosts }) {
   return (
     <div className=" flex">
       <Head>
         <title>博客</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/helogo.png" />
       </Head>
       <div className=" border-gray-100 flex-1">
         <ul className=" p-2.5">
           {posts.map((post) => (
-            <ArtilcleListItem post={post} key={post.uuid}/>
+            <ArtilcleListItem post={post} key={post.uuid} />
           ))}
         </ul>
       </div>
       <div className="w-4/12 hidden relative sm:block">
-       {/* 推荐文章 */}
-       <ArticleRankingList posts={topPosts} />
+        {/* 推荐文章 */}
+        <ArticleRankingList posts={topPosts} />
       </div>
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const res = await fetchArticles({pageSize:100});
-  const topRes = await fetchTopArticles()
+  const res = await fetchArticles({ pageSize: 100 });
+  const topRes = await fetchTopArticles();
   const posts = res?.data?.data || [];
-  const topPosts = topRes?.data || []
+  const topPosts = topRes?.data || [];
   // console.log('top',topPosts)
   // console.log("posts", posts);
   return {
