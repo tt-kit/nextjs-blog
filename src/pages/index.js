961aa252a4cee79be8a7396d13666655c81d7a36
huilong.zhu <zhuhuilong@zj.tech>
2023-05-23 18:17:15 +0800

    feat: 分页

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,27 @@ import Head from "next/head";
 import { fetchArticles, fetchTopArticles } from "@/services";
 import ArtilcleListItem from "@/components/ArticleListItem";
 import ArticleRankingList from "@/components/ArticleRankingList";
-
+import { useEffect, useState } from "react";
+const pageSize = 10;
 export default function Home({ posts, topPosts }) {
+  const [articles, setArticles] = useState(posts);
+  const [pageNo, setPageNo] = useState(1);
+
+  useEffect(() => {
+    if (pageNo > 1) {
+      getArticles();
+    }
+
+    async function getArticles() {
+      const res = await fetchArticles({ pageSize: pageSize, pageNo });
+      const posts = res?.data?.data || [];
+      setArticles([...articles, ...posts]);
+    }
+  }, [pageNo]);
+
+  const onLoadNextPageData = () => {
+    setPageNo(pageNo + 1);
+  };
   return (
     <div className=" flex">
       <Head>
@@ -13,10 +32,16 @@ export default function Home({ posts, topPosts }) {
       </Head>
       <div className=" border-gray-100 flex-1">
         <ul className=" p-2.5">
-          {posts.map((post) => (
+          {articles.map((post) => (
             <ArtilcleListItem post={post} key={post.uuid} />
           ))}
         </ul>
+        <div
+          onClick={onLoadNextPageData}
+          className=" bg-blue-200 p-2 text-center text-gray-500 rounded-md cursor-pointer"
+        >
+          加载更多
+        </div>
       </div>
       <div className="w-4/12 hidden relative sm:block">
         {/* 推荐文章 */}
@@ -27,7 +52,7 @@ export default function Home({ posts, topPosts }) {
 }
 
 export async function getStaticProps() {
-  const res = await fetchArticles({ pageSize: 100 });
+  const res = await fetchArticles({ pageSize: pageSize });
   const topRes = await fetchTopArticles();
   const posts = res?.data?.data || [];
   const topPosts = topRes?.data || [];
--- src/services/index.js
+++ src/services/index.js
@@ -1,36 +1,36 @@
-import fetch from './fetch'
+import fetch from "./fetch";
 
-export const fetchArticles = ({ page = 1, pageSize = 10 } = {}) => {
+export const fetchArticles = ({ pageNo = 1, pageSize = 10 } = {}) => {
   return fetch(`/article`, {
-    pageNo: page,
-    pageSize: pageSize
-  })
-}
+    pageNo: pageNo,
+    pageSize: pageSize,
+  });
+};
 
 /**
  * 推荐文章
  */
 export const fetchTopArticles = () => {
-  return fetch(`/article/top`)
-}
+  return fetch(`/article/top`);
+};
 /**
  * 文章详情
  */
-export const articelDetail = uuid => {
-  return fetch(`/article/detail/${uuid}`)
-}
+export const articelDetail = (uuid) => {
+  return fetch(`/article/detail/${uuid}`);
+};
 
 /**
  * 获取分类标签
  */
 export const fetchTags = () => {
-  return fetch('/tag')
-}
+  return fetch("/tag");
+};
 
 //搜索
-export const search = params => {
-  return fetch('/article/search', params, 'post')
-}
+export const search = (params) => {
+  return fetch("/article/search", params, "post");
+};
 
 /**
  *
@@ -38,33 +38,33 @@ export const search = params => {
  */
 export const commentArticle = (
   _id,
-  { content = '', nickName = '', userEmail = '' } = {}
+  { content = "", nickName = "", userEmail = "" } = {}
 ) => {
   return fetch(
-    '/comment/add',
+    "/comment/add",
     {
       _id: _id,
       content: content,
       nickName: nickName,
-      userEmail: userEmail
+      userEmail: userEmail,
     },
-    'post'
-  )
-}
+    "post"
+  );
+};
 
 /**
  *
  * @param {*} _id
  */
-export const fetchCommentsByArticle = _id => {
-  return fetch('/comment/byArticle', {
-    _id
-  })
-}
+export const fetchCommentsByArticle = (_id) => {
+  return fetch("/comment/byArticle", {
+    _id,
+  });
+};
 
 /**
  * 获取最新文章
  */
 export const fetchLatestArticle = () => {
-  return fetch('/article/latest')
-}
+  return fetch("/article/latest");
+};
